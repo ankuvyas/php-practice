a0054dc5b60cc60d7dc9ccb6d5404159239104f3
Hardik Trivedi <hardik@webmavens.in>
2017-03-05 18:00:57 +0530

    Array Type Tutorial

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Hardik
+	 * Date: 05-03-2017
+	 * Time: 17:56
+	 */
+	$stu[1]['name']="Ankita";
+	$stu[2]['name']="Hardik";
+	$stu[2]['number']=9429572990;
+	$stu[1]['number']=9426629815;
+	$a[1] = $stu;
+	$a[2] = "Hello";
+	echo "<pre>";
+	print_r($a);
+	echo "</pre>";
